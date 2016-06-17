0f99601fc050ec381cc6e737f7ab7f60fdf5ba0a
gabepk <gabepk.ape@gmail.com>
2016-06-17 11:46:09 -0300

    Script para compilacao.

---
+++ compilaTudo.sh
@@ -0,0 +1,6 @@
+# Antes de executar esse script, faça >chmod 777 compilaTudo.sh
+
+gcc alocador.c -o alocador
+gcc substituidor.c -o substituidor
+gcc shutdown.c -o shutdown
+gcc usuario.c -o usuario
